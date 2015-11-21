7941ff5d18eaf6e6edfd05a288bf384e1da70d53
TrầnTrungHiếu_20121699_ASK57 <not_my_name@ymail.com>
2015-11-21 06:58:14 +0000

    Add user microposts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'rails-tutorial-c9-mhartl.c9.io'     # Cloud IDE
+  host = 'rails-tutorial-kagoromo-2.c9.io'     # Cloud IDE
   config.action_mailer.default_url_options = { host: host }
 
   # Print deprecation notices to the Rails logger.
